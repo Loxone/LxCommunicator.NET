25b00fe0afe130734bacaf6eee7ed8aadbc6302b
Kullai Máté <kullai.mate@gmail.com>
2021-04-13 08:05:14 +0200

    Added reference to root issue

--- LxCommunicator.NET/Communicator/Session.cs
+++ LxCommunicator.NET/Communicator/Session.cs
@@ -122,7 +122,7 @@ namespace Loxone.Communicator {
 			Random random = new Random();
 			Salt = string.Concat(Enumerable.Range(0, digits).Select(x => random.Next(16).ToString("X")));
 		}
-		
+
 		/// <summary>
 		/// Converts a string in PEM format to XML format
 		/// </summary>
@@ -131,7 +131,7 @@ namespace Loxone.Communicator {
 		private string PemToXml(string pem) {
 			return GetXmlRsaKey(pem, obj => {
 				var publicKey = (RsaKeyParameters)obj;
-				//return DotNetUtilities.ToRSA(publicKey, new CspParameters { Flags = CspProviderFlags.UseMachineKeyStore });
+				// CspParameters requires Windows. Details: https://github.com/bcgit/bc-csharp/issues/160
 				if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
 					return DotNetUtilities.ToRSA(publicKey, new CspParameters { Flags = CspProviderFlags.UseMachineKeyStore });
 				var parms = DotNetUtilities.ToRSAParameters(publicKey);
